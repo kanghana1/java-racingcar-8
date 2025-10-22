4410d90409269e93f574a1bba18e1d57554cd670
kanghana1 <qazplm12093@naver.com>
2025-10-22 19:37:29 +0900

    feat: 출력 메서드 구현

--- src/main/java/racingcar/OutputPrinter.java
+++ src/main/java/racingcar/OutputPrinter.java
@@ -1,5 +1,11 @@
 package racingcar;
 
 public class OutputPrinter {
-
+    public void printPerRaceResult(Car car) {
+        System.out.print(car.getName() + " : ");
+        for (int i = 0 ; i < car.getWinCount() ; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
